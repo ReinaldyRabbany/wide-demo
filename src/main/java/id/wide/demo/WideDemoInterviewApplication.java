bbf9d0a78d3cd5456a9bcfe1cf94f469eefdd079
Reinaldy Rabbany <reinaldy.vendor.pasti@pegadaian.co.id>
2024-08-25 21:21:32 +0700

    init commit

---
+++ src/main/java/id/wide/demo/WideDemoInterviewApplication.java
@@ -0,0 +1,13 @@
+package id.wide.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WideDemoInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WideDemoInterviewApplication.class, args);
+	}
+
+}
